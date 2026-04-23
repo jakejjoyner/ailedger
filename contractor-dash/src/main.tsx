e65a25a952183abdd8db2b53a44a46e79318eebd
mayor <jakejoyner9@gmail.com>
2026-04-22 19:05:30 -0700

    contractor-dash: page title "<Contractor> - Dashboard"
    Was generic "Contractor". Now sets document.title at startup from
    config.displayName, producing e.g. "Pasha - Dashboard" and
    "Jake (debug) - Dashboard" without any per-contractor code.

--- contractor-dash/src/main.tsx
+++ contractor-dash/src/main.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { config } from "./config";
 import "./index.css";
+
+document.title = `${config.displayName} - Dashboard`;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
